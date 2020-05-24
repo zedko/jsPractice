5df757dc1d1850149a662bf7d5255c90afbdc4af
zedko <zedko@bk.ru>
2020-05-24 14:38:47 +0300

    Решены 1 и 2 задачи.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,69 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
+class ProductsList{
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProduct();
+        this.render();
+    }
+    _fetchProduct(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
 
-// значение по умолчанию - первый объект в массиве products
-// передавая в функцию объект мы упростили (и даже улучшили для дальнейших доработок) запись функции
-const renderProduct = (product = products[0]) => {
-    return `<div class="product-item">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend',productObj.render());
+            //block.innerHTML += productObj.render();
+        }
+    }
+
+    // решение задания 2 / создание метода
+    summary() {
+        let result = 0;
+        this.goods.forEach((item) => result+=item.price)
+        return result;
+    }
+}
+
+class ProductItem{
+    constructor(product, img="https://placehold.it/200x150"){
+        this.title = product.title;
+        this.id = product.id;
+        this.img = img;
+        this.price = product.price;
+    }
+
+    render(){
+        return `<div class="product-item">
+                <img alt="some img" src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    // преобразование productsList в строку удалит запятые
-    document.querySelector('.products').innerHTML = productsList.join("");
-};
+}
 
-renderPage(products);
+//решение задания 1 / создание класса корзины товаров
+class cartList{
+// понадобится метод подсчета суммы корзины
+//     метод добавления предмета в корзину
+//     метод удаления предмета из корзины
+    
+}
+//решение задания 1 / создание класса эелемента корзины товаров
+class cartItem{
+// Я бы делал этот класс на основе ProductItem (class cartItem extends ProductItem)
+//     переопределил бы метод render и, возможно не использовал бы this.img
+}
+let list = new ProductsList();
+// решение задания 2 / вывод
+console.log(list.summary())
